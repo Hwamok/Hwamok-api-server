af2009895e8388168b8517614d59a12ec3884f75
Chulho Shin <chulhoshin@Chulhoui-MacBookPro.local>
2023-11-09 23:17:28 +0900

    first commit

---
+++ src/main/java/com/hwamok/HwamokApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.hwamok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwamokApiServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HwamokApiServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hwamok/HwamokApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwamok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwamokApiServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
