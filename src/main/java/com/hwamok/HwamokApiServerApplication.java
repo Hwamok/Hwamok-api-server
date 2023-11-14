18bd7b3ae7bb9176ce9413ff1d97aa635a857785
kyu <ckdrb0128@naver.com>
2023-11-14 14:39:48 +0900

    slack and git connectionTest

--- src/main/java/com/hwamok/HwamokApiServerApplication.java
+++ src/main/java/com/hwamok/HwamokApiServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HwamokApiServerApplication {
 
   public static void main(String[] args) {
+    System.out.println("슬랙 깃 연결 테스트");
     SpringApplication.run(HwamokApiServerApplication.class, args);
   }
 
