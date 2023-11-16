cb1b3e319a6d2d8dafb58918f0621e4c72373915
goyounha11 <goyounha11@naver.com>
2023-11-16 23:53:57 +0900

    카테고리 테스트 추가

---
+++ src/test/java/com/hwamok/category/domain/CategoryTest.java
@@ -0,0 +1,47 @@
+package com.hwamok.category.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+  @Test
+  void 카테고리_생성_성공() {
+    Category category = new Category("화목한쇼핑몰", "CA001", "식품", 0, null);
+
+    assertThat(category.getBranch()).isEqualTo("화목한쇼핑몰");
+    assertThat(category.getCode()).isEqualTo("CA001");
+    assertThat(category.getName()).isEqualTo("식품");
+    assertThat(category.getLevel()).isEqualTo(0);
+    assertThat(category.getParentCategory()).isNull();
+  }
+
+  @Test
+  void 하위_카테고리_생성_성공() {
+    Category parentCategory = new Category("화목한쇼핑몰", "CA001", "식품", 0, null);
+
+    Category subCategory = new Category("화목한쇼핑몰", "CA002", "가공식품", 1, parentCategory);
+
+    assertThat(subCategory.getBranch()).isEqualTo("화목한쇼핑몰");
+    assertThat(subCategory.getCode()).isEqualTo("CA002");
+    assertThat(subCategory.getName()).isEqualTo("가공식품");
+    assertThat(subCategory.getLevel()).isEqualTo(1);
+    assertThat(subCategory.getParentCategory()).isSameAs(parentCategory);
+  }
+
+  @Test
+  void 부모_카테고리로_서브_카테고리들_조회() {
+    Category parentCategory = new Category("화목한쇼핑몰", "CA001", "식품", 0, null);
+
+    Category subCategory1 = new Category("화목한쇼핑몰", "CA002", "가공식품", 1, parentCategory);
+    Category subCategory2 = new Category("화목한쇼핑몰", "CA003", "신선식품", 1, parentCategory);
+
+    parentCategory.getSubCategory().add(subCategory1);
+    parentCategory.getSubCategory().add(subCategory2);
+
+    assertThat(parentCategory.getSubCategory().size()).isEqualTo(2);
+    assertThat(parentCategory.getSubCategory()).containsExactly(subCategory1, subCategory2);
+  }
+}
