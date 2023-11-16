921da608384e62a5fea9067068d76f4f613ff64d
LeeAYoung1207 <elluan3369@gmail.com>
2023-11-16 23:07:19 +0900

    HM-7 category 도메인 개발

--- src/main/java/com/hwamok/category/domain/Category.java
+++ src/main/java/com/hwamok/category/domain/Category.java
@@ -4,6 +4,7 @@ import com.hwamok.product.domain.Product;
 import com.hwamok.support.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -29,4 +30,21 @@ public class Category extends BaseEntity {
 
   @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
   private List<Product> products = new ArrayList<>();
+
+  public void setParentCategory(Category parentCategory){
+    this.parentCategory = parentCategory;
+  }
+  public void setLevel(Integer level){
+    this.level = level;
+  }
+
+
+  @Builder
+  public Category(String branch, String code, String name, Integer level, Category parentCategory){
+    this.branch = branch;
+    this.code = code;
+    this.name = name;
+    this.level = level;
+    this.parentCategory = parentCategory;
+  }
 }
