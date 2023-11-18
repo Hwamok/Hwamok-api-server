eb4a4a3690b87addcd8c68171087271dacbcd970
LeeAYoung1207 <elluan3369@gmail.com>
2023-11-18 17:54:02 +0900

    HM-7 category 도메인 수정

--- src/main/java/com/hwamok/category/domain/Category.java
+++ src/main/java/com/hwamok/category/domain/Category.java
@@ -1,12 +1,16 @@
 package com.hwamok.category.domain;
 
+import com.hwamok.core.exception.ExceptionCode;
+import com.hwamok.core.exception.HwamokException;
 import com.hwamok.product.domain.Product;
 import com.hwamok.support.BaseEntity;
+import com.hwamok.utils.PreConditions;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,8 +19,15 @@ import java.util.List;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category extends BaseEntity {
+  //유효성검사, 필수값 검증
+
+  @Column(length = 30, nullable = false)
   private String branch;
+
+  @Column(length = 5, nullable = false)
   private String code;
+
+  @Column(length = 30, nullable = false)
   private String name;
 
   @ManyToOne(fetch = FetchType.LAZY)
@@ -26,21 +37,33 @@ public class Category extends BaseEntity {
   @OneToMany(mappedBy = "parentCategory", cascade = CascadeType.ALL)
   private List<Category> subCategory = new ArrayList<>();
 
+  @Column(nullable = false)
   private Integer level;
 
   @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
   private List<Product> products = new ArrayList<>();
 
-  public void setParentCategory(Category parentCategory){
+  public void registerParentCategory(Category parentCategory){
     this.parentCategory = parentCategory;
   }
-  public void setLevel(Integer level){
+  public void registerLevel(Integer level){
     this.level = level;
   }
 
 
   @Builder
   public Category(String branch, String code, String name, Integer level, Category parentCategory){
+    PreConditions.require(Strings.isNotBlank(branch));
+    PreConditions.require(Strings.isNotBlank(code));
+    PreConditions.require(Strings.isNotBlank(name));
+    //String으로 넣어서 validate로 숫자만 넣는것을 확인할지, 혹은 int로 할지...
+    PreConditions.require(level != null);
+
+    PreConditions.validate(branch.length() < 31, ExceptionCode.NOT_BRANCH_FORM);
+    PreConditions.validate(code.length() < 6, ExceptionCode.NOT_CODE_FORM);
+    PreConditions.validate(name.length() < 31, ExceptionCode.NOT_NAME_FORM);
+    PreConditions.validate(level > -1, ExceptionCode.NOT_LEVEL_FORM);
+
     this.branch = branch;
     this.code = code;
     this.name = name;
--- src/test/java/com/hwamok/category/domain/CategoryTest.java
+++ src/test/java/com/hwamok/category/domain/CategoryTest.java
@@ -1,7 +1,12 @@
 package com.hwamok.category.domain;
 
+import com.hwamok.core.exception.ExceptionCode;
+import com.hwamok.core.exception.HwamokExceptionTest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,14 +39,72 @@ class CategoryTest {
   @Test
   void 부모_카테고리로_서브_카테고리들_조회() {
     Category parentCategory = new Category("화목한쇼핑몰", "CA001", "식품", 0, null);
-
     Category subCategory1 = new Category("화목한쇼핑몰", "CA002", "가공식품", 1, parentCategory);
     Category subCategory2 = new Category("화목한쇼핑몰", "CA003", "신선식품", 1, parentCategory);
-
     parentCategory.getSubCategory().add(subCategory1);
     parentCategory.getSubCategory().add(subCategory2);
 
     assertThat(parentCategory.getSubCategory().size()).isEqualTo(2);
     assertThat(parentCategory.getSubCategory()).containsExactly(subCategory1, subCategory2);
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void 카테고리_생성_실패__브랜치_값_없음(String branch) {
+    Assertions.assertThatIllegalArgumentException().isThrownBy(() ->
+            new Category(branch, "CA001", "식품", 0, null));
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void 카테고리_생성_실패__코드_값_없음(String code) {
+    Assertions.assertThatIllegalArgumentException().isThrownBy(() ->
+            new Category("화목한쇼핑몰", code, "식품", 0, null));
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void 카테고리_생성_실패__이름_값_없음(String name) {
+    Assertions.assertThatIllegalArgumentException().isThrownBy(() ->
+            new Category("화목한쇼핑몰", "CA001", name, 0, null));
+  }
+
+  @ParameterizedTest
+  @NullSource
+  void 카테고리_생성_실패__레벨_값_없음(Integer level) {
+    Assertions.assertThatIllegalArgumentException().isThrownBy(() ->
+            new Category("화목한쇼핑몰", "CA001", "식품", level, null));
+  }
+
+  @Test
+  void 카테고리_생성_실패__브랜치_31글자_이상() {
+    String wrongBranch = "화목한쇼핑몰화목한쇼핑몰화목한쇼핑몰화목한쇼핑몰화목한쇼핑몰화";
+
+    HwamokExceptionTest.assertThatHwamokException(ExceptionCode.NOT_BRANCH_FORM)
+            .isThrownBy(()-> new Category(wrongBranch, "CA001", "식품", 0, null));
+  }
+
+  @Test
+  void 카테고리_생성_실패__코드_7글자_이상() {
+    String wrongCode = "CA77777";
+
+    HwamokExceptionTest.assertThatHwamokException(ExceptionCode.NOT_CODE_FORM)
+            .isThrownBy(()-> new Category("화목한쇼핑몰", wrongCode, "식품", 0, null));
+  }
+
+  @Test
+  void 카테고리_생성_실패__이름_31글자_이상() {
+    String wrongName = "식품식품식품식품식품식품식품식품식품식품식품식품식품식품식품식";
+
+    HwamokExceptionTest.assertThatHwamokException(ExceptionCode.NOT_NAME_FORM)
+            .isThrownBy(()-> new Category("화목한쇼핑몰", "CA001", wrongName, 0, null));
+  }
+
+  @Test
+  void 카테고리_생성_실패__레벨_음수() {
+    Integer level = -1;
+
+    HwamokExceptionTest.assertThatHwamokException(ExceptionCode.NOT_LEVEL_FORM)
+            .isThrownBy(()-> new Category("화목한쇼핑몰", "CA001", "식품", level, null));
   }
 }
