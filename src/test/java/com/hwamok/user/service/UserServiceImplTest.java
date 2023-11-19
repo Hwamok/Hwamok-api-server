bafa0c8e6f4ffe022a33297a600361825c2a9762
Glenn Jeong <jyb1624@gmail.com>
2023-11-19 14:34:31 +0900

    HM-18 User 서비스 개발 및 테스트 코딩 작성 중

--- src/test/java/com/hwamok/user/service/UserServiceImplTest.java
+++ src/test/java/com/hwamok/user/service/UserServiceImplTest.java
@@ -165,142 +165,270 @@ class UserServiceImplTest {
     @ParameterizedTest
     @NullAndEmptySource
     void 회원_가입_실패_addr_혹은_빈값(String addr) {
-
+        assertThatIllegalArgumentException()
+                .isThrownBy(()->userRepository.save(new User("hwamok@test.com", "1234",
+                        "hwamok", "2023-11-15", "01012345678", "GOOGLE", "originalImage",
+                        "savedImage",12345, addr,
+                        "201")));
     }
 
     @ParameterizedTest
     @NullAndEmptySource
     void 회원_가입_실패_detailAddr_혹은_빈값(String detailAddr) {
-
+        assertThatIllegalArgumentException()
+                .isThrownBy(()->userRepository.save(new User("hwamok@test.com", "1234",
+                        "hwamok", "2023-11-15", "01012345678", "GOOGLE", "originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        detailAddr)));
     }
 
     @Test
     void 회원_가입_실패_email_50글자_초과() {
         String fakeEmail = "hwamokhwamokhwamokhwamokhwamokhwamokhwamokhwamokhwamokhwamokhwamok@test.com";
 
-
+        assertThatIllegalArgumentException()
+                .isThrownBy(()-> userRepository.save(new User(fakeEmail, "1234", "hwamok",
+                        "2023-11-15", "01012345678", "GOOGLE","originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_name_20글자_초과() {
         String fakeName = "hwamokhwamokhwamokhwamok";
 
+        assertThatIllegalArgumentException()
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", fakeName,
+                        "2023-11-15", "01012345678", "GOOGLE","originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_birthDay_10글자_초과() {
         String fakeBirthDay = "2023-11-1512";
 
+        assertThatIllegalArgumentException()
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        fakeBirthDay, "01012345678", "GOOGLE","originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_phone_11글자_초과() {
         String fakePhone = "010123456789";
 
+        assertThatIllegalArgumentException()
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        "2023-11-15", fakePhone, "GOOGLE","originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_platform_11글자_초과() {
         String fakePlatform = "GOOGLEGOOGLE";
 
+        assertThatIllegalArgumentException()
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        "2023-11-15", "01012345678", fakePlatform,"originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
+    }
+
+    @Test
+    void 회원_가입_실패_addr_80글자_초과() {
+        String fakeAddr = "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea, 15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea";
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        "2023-11-15", "01012345678", "GOOGLE", "originalImage",
+                        "savedImage",12345, fakeAddr,"201")));
+    }
+
+    @Test
+    void 회원_가입_실패_detail_10글자_초과() {
+        String fakeDetail = "10203405123";
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        "2023-11-15", "01012345678", "GOOGLE", "originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        fakeDetail)));
     }
 
     @Test
     void 회원_가입_실패_validate_name_특수문자() {
         String fakeName = "화목!";
 
+        assertThatHwamokException(ExceptionCode.NOT_NAME_FORM)
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", fakeName,
+                        "2023-11-15", "01012345678", "GOOGLE","originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_validate_name_두_글자_미만() {
         String fakeName = "화";
 
+        assertThatHwamokException(ExceptionCode.NOT_NAME_FORM)
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", fakeName,
+                        "2023-11-15", "01012345678", "GOOGLE","originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_validate_name_영문한글_혼용() {
         String fakeName = "화목hwamok";
 
+        assertThatHwamokException(ExceptionCode.NOT_NAME_FORM)
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", fakeName,
+                        "2023-11-15", "01012345678", "GOOGLE","originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_validate_email_골뱅이_없음() {
         String fakeEmail = "hwamoktest.com";
 
+        assertThatHwamokException(ExceptionCode.NOT_EMAIL_FORM)
+                .isThrownBy(()-> userRepository.save(new User(fakeEmail, "1234", "hwamok",
+                        "2023-11-15", "01012345678", "GOOGLE","originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_validate_email_점_없음() {
         String fakeEmail = "hwamok@testcom";
 
-
+        assertThatHwamokException(ExceptionCode.NOT_EMAIL_FORM)
+                .isThrownBy(()-> userRepository.save(new User(fakeEmail, "1234", "hwamok",
+                        "2023-11-15", "01012345678", "GOOGLE","originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_validate_birthDay_다시_없음() {
         String fakeBirthDay = "20231115";
 
-
+        assertThatHwamokException(ExceptionCode.NOT_DATE_FORM)
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        fakeBirthDay, "01012345678", "GOOGLE","originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_validate_birthDay_슬래시_변경() {
         String fakeBirthDay = "2023/11/15";
 
-
+        assertThatHwamokException(ExceptionCode.NOT_DATE_FORM)
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        fakeBirthDay, "01012345678", "GOOGLE","originalImage",
+                        "savedImage",12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_post_음수일때() {
         int fakePost = -1;
 
+        assertThatIllegalArgumentException()
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        "2023-11-15", "01012345678", "GOOGLE","originalImage",
+                        "savedImage", fakePost, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_validate_post_5자리_아래일때() {
         int fakePost = 1234;
 
-
+        assertThatHwamokException(ExceptionCode.NOT_POST_FORM)
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        "2023-11-15", "01012345678", "GOOGLE","originalImage",
+                        "savedImage", fakePost, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_validate_phone_숫자_제외_다른_문자() {
         String fakePhone = "010-1234#56";
 
-
+        assertThatHwamokException(ExceptionCode.NOT_PHONE_FORM)
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        "2023-11-15", fakePhone, "GOOGLE","originalImage",
+                        "savedImage", 12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_validate_phone_11자리_미만() {
         String fakePhone = "0101234567";
 
-
+        assertThatHwamokException(ExceptionCode.NOT_PHONE_FORM)
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        "2023-11-15", fakePhone, "GOOGLE","originalImage",
+                        "savedImage", 12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_validate_phone_첫째자리_0_제외() {
         String fakePhone = "21012345678";
 
-
+        assertThatHwamokException(ExceptionCode.NOT_PHONE_FORM)
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        "2023-11-15", fakePhone, "GOOGLE","originalImage",
+                        "savedImage", 12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @Test
     void 회원_가입_실패_validate_phone_두째자리_1_제외() {
         String fakePhone = "00012345678";
 
-
+        assertThatHwamokException(ExceptionCode.NOT_PHONE_FORM)
+                .isThrownBy(()-> userRepository.save(new User("hwamok@test.com", "1234", "hwamok",
+                        "2023-11-15", fakePhone, "GOOGLE","originalImage",
+                        "savedImage", 12345, "15, Deoksugung-gil, Jung-gu, Seoul, Republic of Korea",
+                        "201")));
     }
 
     @ParameterizedTest
     @NullAndEmptySource
     void 회원_수정_실패_email_null_혹은_빈값(String email) {
+        User user = userRepository.save(UserFixture.create());
 
+        User foundId = userRepository.findById(user.getId())
+                .orElseThrow(()-> new HwamokException(ExceptionCode.NOT_FOUND_USER));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(()-> userService.update(foundId.getId(), email, "12345", "hwamokhwa",
+                "2023-11-16", "01012345679", "NAVER", "originalImage1",
+                "savedImage1",12346, "17, Deoksugung-gil1, Jung-gu1, Seoul, Republic of Korea",
+                "202"));
     }
 
     @ParameterizedTest
     @NullAndEmptySource
     void 회원_수정_실패_password_null_혹은_빈값(String password) {
+        User user = userRepository.save(UserFixture.create());
 
+        User foundId = userRepository.findById(user.getId())
+                .orElseThrow(()-> new HwamokException(ExceptionCode.NOT_FOUND_USER));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(()-> userService.update(foundId.getId(), "hwamok@test.com", password, "hwamokhwa",
+                        "2023-11-16", "01012345679", "NAVER", "originalImage1",
+                        "savedImage1",12346, "17, Deoksugung-gil1, Jung-gu1, Seoul, Republic of Korea",
+                        "202"));
     }
 
     @ParameterizedTest
