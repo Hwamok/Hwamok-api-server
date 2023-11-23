a72274ff98d55f8e23020a62fec0c4243d4a3522
Glenn Jeong <jyb1624@gmail.com>
2023-11-23 13:45:52 +0900

    HM-18 User 서비스 비밀번호 암호화 추가

---
+++ src/main/generated/com/hwamok/product/domain/QProduct.java
@@ -0,0 +1,65 @@
+package com.hwamok.product.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QProduct is a Querydsl query type for Product
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QProduct extends EntityPathBase<Product> {
+
+    private static final long serialVersionUID = -862234610L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QProduct product = new QProduct("product");
+
+    public final com.hwamok.support.QBaseEntity _super = new com.hwamok.support.QBaseEntity(this);
+
+    public final com.hwamok.category.domain.QCategory category;
+
+    public final StringPath code = createString("code");
+
+    //inherited
+    public final DateTimePath<java.time.Instant> createdAt = _super.createdAt;
+
+    //inherited
+    public final NumberPath<Long> id = _super.id;
+
+    public final StringPath name = createString("name");
+
+    public final NumberPath<Integer> price = createNumber("price", Integer.class);
+
+    public final EnumPath<ProductStatus> status = createEnum("status", ProductStatus.class);
+
+    public QProduct(String variable) {
+        this(Product.class, forVariable(variable), INITS);
+    }
+
+    public QProduct(Path<? extends Product> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QProduct(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QProduct(PathMetadata metadata, PathInits inits) {
+        this(Product.class, metadata, inits);
+    }
+
+    public QProduct(Class<? extends Product> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.category = inits.isInitialized("category") ? new com.hwamok.category.domain.QCategory(forProperty("category"), inits.get("category")) : null;
+    }
+
+}
+
